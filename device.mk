74dc95ea3be8c19a6ffbbee768b694a0af44ec1e
WenHao2130 <wenhao2130@outlook.com>
2025-11-22 20:10:32 +0800

    OP4E9F: Build missing libhistogram
    - Commit 4475200560ccce0d15090b6501022ba10c8eb54d droped it, but not
      build it
    
    Change-Id: I0365a740957a87aa17e0421585ee2d2413d3b176
    Signed-off-by: WenHao2130 <wenhao2130@outlook.com>

--- device.mk
+++ device.mk
@@ -129,6 +129,7 @@ PRODUCT_PACKAGES += \
     gralloc.lito \
     libdisplayconfig.qti \
     libdisplayconfig.system.qti \
+    libhistogram \
     libqdMetaData \
     libqdMetaData.system \
     libqdutils \
