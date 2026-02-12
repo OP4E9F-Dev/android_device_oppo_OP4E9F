9c93403a3b024b7529742e88daece95c5300152c
adbzxcghf <adbzxcghf@outlook.com>
2026-02-12 20:16:14 +0800

    OP4E9F: Use hardware/oplus/sepolicy/qti
    Signed-off-by: adbzxcghf <adbzxcghf@outlook.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -159,6 +159,7 @@ VENDOR_SECURITY_PATCH := 2025-05-01
 
 # Sepolicy
 include device/qcom/sepolicy_vndr-legacy-um/SEPolicy.mk
+include hardware/oplus/sepolicy/qti/SEPolicy.mk
 
 # Treble
 BOARD_VNDK_VERSION := current
